9cb8e4d498e804ffd5e55fa0bd3bc09528a70955
NatalyM <nbelokopytova@gmail.com>
2022-11-29 14:56:02 +0300

    scroll v 1.0

--- src/test/java/app/App_test.java
+++ src/test/java/app/App_test.java
@@ -1,13 +1,19 @@
 package app;
 
 import asserts.AssertUser;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import extensions.AppiumExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import pages.*;
 
+import java.time.Duration;
 import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.$;
 
 //clean test -DforkСount=0 -Dtest=App_test
 
@@ -18,6 +24,35 @@ public class App_test {
   private UserPage userPage = new UserPage();
   private PostPage postPage = new PostPage();
   private PostsPage postsPage = new PostsPage();
+
+  @Test
+  public void scrollTest(){
+    mainPage.open();
+
+    String userId = "3";
+
+    Map<String,String> user = usersPage.getUserInfoFromJson(userId);
+    String name = user.get("name");
+    String username = user.get("username");
+
+    SelenideElement userElement = userPage.findUser(username);
+
+
+    PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+    Sequence longpress = new Sequence(finger, 1);
+    longpress.addAction(
+            finger.createPointerMove(Duration.ofMillis(0),
+                    PointerInput.Origin.viewport(),
+                    userElement.getLocation().x,
+                    userElement.getLocation().y)
+                //    $("[content-desc *= 'Tab 2 of 2']").getLocation().x,
+                //    $("[content-desc *= 'Tab 2 of 2']").getLocation().y)
+    );
+    for (int i = 0; i < 3; i++) {
+      Selenide.sleep(5000);
+      System.out.println("sleep.."+i);
+    }
+  }
 
   //Тест, который кликает по пользователю с заданным ID = (1,2,3) и проверяет, что отобразилась информация именно по этому пользователю
   @Test
